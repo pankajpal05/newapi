5ecd5e09094145a7680f1bd3c2908fdfe616a526
pankaj pal <pankaj.pal@ayasya.com>
2024-04-04 11:58:32 +0530

    Update code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import About from './components/About'
-import Navbar from './components/Navbar'
-import TopStories from './components/TopStories'
-import NotFound from './components/NotFound'
-import Footer from './components/Footer'
-import Search from './components/Search'
-import {categories} from './constants/index'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TopStories from "./components/TopStories";
+import NotFound from "./components/NotFound";
+import Footer from "./components/Footer";
+import Search from "./components/Search";
+import { CATEGORIES } from "./constants/index";
 
 function App() {
-  const apiKey = "Put Your Own";
+  const apiKey = "oxr3v00wJywPZwi0PVUxZyw8OmMc6i9a";
 
   return (
     <>
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path='/' element={<TopStories apiKey={apiKey} section="home" />}></Route>
-          <Route exact path='/about' element={<About />}></Route>
-          <Route exact path='*' element={<NotFound />}></Route>
-          <Route exact path='/search' element={<Search apiKey={apiKey}/>}></Route>
-          {
-            categories.map((e) => {
-              return <Route key={e} exact path={`/categories/${e.toLowerCase()}`} element={<TopStories apiKey={apiKey} section={e.toLowerCase()} />}></Route>
-            })
-          }
+          <Route
+            exact
+            path="/"
+            element={<TopStories apiKey={apiKey} section="home" />}
+          ></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="*" element={<NotFound />}></Route>
+          <Route
+            exact
+            path="/search"
+            element={<Search apiKey={apiKey} />}
+          ></Route>
+          {CATEGORIES.map((e) => {
+            return (
+              <Route
+                key={e}
+                exact
+                path={`/categories/${e.toLowerCase()}`}
+                element={
+                  <TopStories apiKey={apiKey} section={e.toLowerCase()} />
+                }
+              ></Route>
+            );
+          })}
         </Routes>
         <Footer />
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {categories} from '../constants/index'
+import { CATEGORIES } from "../constants/index";
 
 function Navbar() {
 
@@ -23,52 +23,120 @@ function Navbar() {
     <>
       <nav className="navbar navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">News Wallah</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+          <a className="navbar-brand" href="/">
+            News Wallah
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasDarkNavbar"
+            aria-controls="offcanvasDarkNavbar"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <div
+            className="offcanvas offcanvas-end text-bg-dark"
+            tabIndex="-1"
+            id="offcanvasDarkNavbar"
+            aria-labelledby="offcanvasDarkNavbarLabel"
+          >
             <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">News Wallah</h5>
-              <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close" ref={close}></button>
+              <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                News Wallah
+              </h5>
+              <button
+                type="button"
+                className="btn-close btn-close-white"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+                ref={close}
+              ></button>
             </div>
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Home</a>
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Home
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">About Us</a>
+                  <a className="nav-link" href="/about">
+                    About Us
+                  </a>
                 </li>
                 <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="/"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
                     Categories
                   </a>
                   <ul className="dropdown-menu dropdown-menu-dark">
-                    {
-                      categories.map((e) => {
-                        if (e === "RealEstate") {
-                          return <li key={e}><a className="dropdown-item" href={`/categories/${e.toLowerCase()}`}>Real Estate</a></li>
-                        }
-                        else if (e === "Sundayreview") {
-                          return <li key={e}><a className="dropdown-item" href={`/categories/${e.toLowerCase()}`}>Sunday Opinion</a></li>
-                        }
-                        return <li key={e}><a className="dropdown-item" href={`/categories/${e.toLowerCase()}`}>{e}</a></li>
-                      })
-                    }
+                    {CATEGORIES.map((e) => {
+                      if (e === "RealEstate") {
+                        return (
+                          <li key={e}>
+                            <a
+                              className="dropdown-item"
+                              href={`/categories/${e.toLowerCase()}`}
+                            >
+                              Real Estate
+                            </a>
+                          </li>
+                        );
+                      } else if (e === "Sundayreview") {
+                        return (
+                          <li key={e}>
+                            <a
+                              className="dropdown-item"
+                              href={`/categories/${e.toLowerCase()}`}
+                            >
+                              Sunday Opinion
+                            </a>
+                          </li>
+                        );
+                      }
+                      return (
+                        <li key={e}>
+                          <a
+                            className="dropdown-item"
+                            href={`/categories/${e.toLowerCase()}`}
+                          >
+                            {e}
+                          </a>
+                        </li>
+                      );
+                    })}
                   </ul>
                 </li>
               </ul>
-              <form className="d-flex mt-3" role="search" onSubmit={handleSubmit}>
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" name='search' onChange={handleChange} />
-                <button className="btn btn-success" type="submit">Search</button>
+              <form
+                className="d-flex mt-3"
+                role="search"
+                onSubmit={handleSubmit}
+              >
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  name="search"
+                  onChange={handleChange}
+                />
+                <button className="btn btn-success" type="submit">
+                  Search
+                </button>
               </form>
             </div>
           </div>
         </div>
       </nav>
     </>
-  )
+  );
 }
 
 export default Navbar
